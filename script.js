4e1b332f37a0e66dd86cd50a0baf16d272bde4c8
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-06-13 18:18:48 +0200

    Les-Petits-plats

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Il importe et utilise des fonctions depuis d'autres modules pour afficher et filtrer les données.
 
 // Importation de la fonction displayData depuis le fichier display.js
-import { displayData, SelectFilterTagIngredients} from "./js/display.js";
+import { displayData, SelectFilterTagIngredients , SelectFilterTagUstensiles ,SelectFilterTagAppliance} from "./js/display.js";
 
 // Importation de la fonction fetchData depuis le fichier api.js
 import { fetchData } from "./js/api.js";
@@ -10,8 +10,8 @@ import { fetchData } from "./js/api.js";
 // Importation de la fonction filtrageInput
 import { filtrageInput } from "./js/filtrage.js";
 
-//// Importation de la fonction filtrageTaginput
-import { filtreTagIngredient } from "./js/filtrageTag.js";
+// Importation de la fonction filtrageTaginput
+import { filtreTagIngredient ,filtreTagUstensiles ,filtreTagAppliance } from "./js/filtrageTag.js";
 
 
 
@@ -29,7 +29,8 @@ function init() {
   // Display initial data
   displayData(globalData);
   SelectFilterTagIngredients(globalData);
-
+  SelectFilterTagUstensiles(globalData);
+  SelectFilterTagAppliance(globalData);
   // Event listener for main input button
   boutonInputHeader.addEventListener("click", () => {
     // Update the filtered data from main input
@@ -65,7 +66,7 @@ function init() {
 
 
 
-  const inputSubit = document.querySelectorAll(".loupe-for-input");
+  const inputSubit = document.querySelectorAll(".loupe-for-input"); // on recupere les loupes pour subit les tags
   inputSubit[0].addEventListener("click", () => {
     // Filter tags based on the current main input filtered data
     const dataToFilter = filteredDataMainInput.length === 0 ? globalData : filteredDataMainInput;
@@ -79,19 +80,33 @@ function init() {
       SelectFilterTagIngredients(dataToFilter, filteredDataInputTags);
     }
   });
-  // inputSubit[1].addEventListener("click", () => {
-  //   // Filter tags based on the current main input filtered data
-  //   const dataToFilter = filteredDataMainInput.length === 0 ? globalData : filteredDataMainInput;
-  //   filteredDataInputTags = filtreTagIngredient(dataToFilter);
+  inputSubit[1].addEventListener("click", () => {
+    // Filter tags based on the current main input filtered data
+    const dataToFilter = filteredDataMainInput.length === 0 ? globalData : filteredDataMainInput;
+    filteredDataInputTags = filtreTagUstensiles(dataToFilter);
 
-  //   if (filteredDataInputTags.length === 0) {
-  //     // If no tags were filtered, use the current main input filtered data
-  //     SelectFilterTagIngredients(dataToFilter);
-  //   } else {
-  //     // Display filtered tags
-  //     SelectFilterTagIngredients(dataToFilter, filteredDataInputTags);
-  //   }
-  // });
+    if (filteredDataInputTags.length === 0) {
+      // If no tags were filtered, use the current main input filtered data
+      SelectFilterTagUstensiles(dataToFilter);
+    } else {
+      // Display filtered tags
+      SelectFilterTagUstensiles(dataToFilter, filteredDataInputTags);
+    }
+  });
+
+  inputSubit[2].addEventListener("click", () => {
+    // Filter tags based on the current main input filtered data
+    const dataToFilter = filteredDataMainInput.length === 0 ? globalData : filteredDataMainInput;
+    filteredDataInputTags = filtreTagAppliance(dataToFilter);
+
+    if (filteredDataInputTags.length === 0) {
+      // If no tags were filtered, use the current main input filtered data
+      SelectFilterTagAppliance(dataToFilter);
+    } else {
+      // Display filtered tags
+      SelectFilterTagAppliance(dataToFilter, filteredDataInputTags);
+    }
+  });
 
 
 
